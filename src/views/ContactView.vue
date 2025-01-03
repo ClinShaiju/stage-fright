0a44c081aa01396cd3a95c1c21475fcd9ad5b5e3
Joshua Zamora <180025124+joshmzamora@users.noreply.github.com>
2025-01-02 22:46:43 -0600

    Changed page content from "Contact Us" to "Contact"

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,34 +1,9 @@
-<script>
-import { ref, onMounted, onUnmounted } from 'vue';
+<script setup>
 
-export default {
-  setup() {
-    const windowWidth = ref(window.innerWidth);
-    const windowHeight = ref(window.innerHeight);
-
-    const updateWindowSize = () => {
-      windowWidth.value = window.innerWidth;
-      windowHeight.value = window.innerHeight;
-    };
-
-    onMounted(() => {
-      window.addEventListener('resize', updateWindowSize);
-    });
-
-    onUnmounted(() => {
-      window.removeEventListener('resize', updateWindowSize);
-    });
-
-    return {
-      windowWidth,
-      windowHeight,
-    };
-  },
-};
 </script>
 
 <template>
   <div class="bg-black h-dvh flex">
-    <h1 class="flex text-white h-dvh w-dvw items-center justify-center text-3xl">Contact Us</h1>
+    <h1 class="flex text-white h-dvh w-dvw items-center justify-center text-3xl">Contact</h1>
   </div>
 </template>

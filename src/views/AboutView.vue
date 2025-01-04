42e9f12aded8a6c8c17304b9ec0074ca0c756055
Joshua Zamora <180025124+joshmzamora@users.noreply.github.com>
2025-01-04 17:21:18 -0600

    Add Contact Us page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,16 @@
 </script>
 
 <template>
-  <div class="bg-black h-dvh flex">
-    <h1 class="flex text-white h-dvh w-dvw items-center justify-center text-3xl">About Us</h1>
+  <div class="flex flex-col bg-black min-h-dvh items-center">
+    <div class="h-[80px]"></div>
+    <h1 class="flex text-white font-thedark items-center justify-center text-6xl">
+      About Us
+    </h1>
+    <p class="flex text-white text-center p-3">
+      Stage Fright is made of four members (Paul Carter, Gene Martin, Ace Kinley, and Peter Winters). The boys formed the band during their freshman year of high school, and they first started making music in Ace’s garage. After many months of practice, the boys booked their first gig at the beginning of their sophomore year at their favorite local restaurant (Don’s Barbeque: Tampa, FL) and have been performing ever since.
+    </p>
+    <div class="h-[80px]"></div>
+    <div class=" w-32 h-32 bg-gray-700 rounded-lg mb-4"></div>
+
   </div>
 </template>
--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,9 +1,68 @@
-<script setup>
-
-</script>
-
 <template>
-  <div class="bg-black h-dvh flex">
-    <h1 class="flex text-white h-dvh w-dvw items-center justify-center text-3xl">Contact</h1>
+  <div class="flex flex-col bg-neutral-950 items-center min-h-dvh ">
+    <div class="h-[80px]"></div>
+    <div class="mt-8 flex flex-col items-center justify-end ">
+      <h1 class="flex text-white h-auto w-dvw text-6xl justify-center  font-thedark">
+        Contact Us
+      </h1>
+      <h2 class="flex text-white h-auto text-1.5xl w-dvw justify-center  font-loud">
+        Leave a message below or contact us directly!
+      </h2>
+    </div>
+
+    <div class="flex flex-col-reverse  w-dvw max-w-7xl  md:flex-row  ">
+      <div class="flex flex-col w-full md:w-1/2  p-6 text-center">
+        <h2 class="flex text-6xl text-white font-thedark justify-center">Online Inquiry</h2>
+        <h2 class="flex text-xl text-white font-thin justify-center mt-3 ">Our team will get back to you soon!</h2>
+        <form class="flex flex-col items-center m-4 gap-4">
+          <input
+            id="name"
+            type="text"
+            class="block  rounded-md p-2 text-black w-full"
+            placeholder="Name"
+          />
+
+          <input
+            id="name"
+            type="text"
+            class="block  rounded-md p-2 text-black w-full"
+            placeholder="Email"
+          />
+
+          <textarea
+            id="message"
+            type="text"
+            class="block  rounded-md p-2 text-black w-full h-32 md:h-40 "
+            placeholder="Message"
+          ></textarea>
+
+          <button
+            type="submit"
+            class="transition-all hover:scale-[1.1] px-24 py-2.5 text-xl text-white font-semibold rounded-full border border-white hover:text-white hover:bg-red-700 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 w-auto"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+      <div class="w-full md:w-1/2 p-6 text-center">
+        <h2 class="flex text-6xl text-white font-thedark justify-center">Contact Details</h2>
+        <div class="mb-4">
+          <h2 class="flex text-2xl text-white justify-center font-bold mb-1 mt-3">Support</h2>
+          <h2 class="flex text-xl text-white font-thin justify-center hover:underline hover:text-red-600 hover:font-bold transition-all"><a href="mailto:support@stagefrightrock.com" title="Stage Fright's support email">support@stagefrightrock.com</a></h2>
+        </div>
+        <div class="mb-4">
+          <h2 class="flex text-2xl text-white justify-center font-bold mb-1">Feedback</h2>
+          <h2 class="flex text-xl text-white font-thin justify-center hover:underline hover:text-red-600 hover:font-bold transition-all"><a href="mailto:feedback@stagefrightrock.com" title="Stage Fright's support email">feedback@stagefrightrock.com</a></h2>
+        </div>
+        <div class="mb-4">
+          <h2 class="flex text-2xl text-white justify-center font-bold mb-1">Booking</h2>
+          <h2 class="flex text-xl text-white font-thin justify-center hover:underline hover:text-red-600 hover:font-bold transition-all"><a href="mailto:booking@stagefrightrock.com" title="Stage Fright's support email">booking@stagefrightrock.com</a></h2>
+        </div>
+        <div class="mb-4">
+          <h2 class="flex text-2xl text-white justify-center font-bold mb-1">Phone Number</h2>
+          <h2 class="flex text-xl text-white font-thin justify-center hover:underline hover:text-red-600 hover:font-bold transition-all"><a href="tel:+8329842275" title="Stage Fright's phone number">(832) 329-ROCK</a> </h2>
+        </div>
+      </div>
+    </div>
   </div>
 </template>

fb37d1bd0ceabfd77590ea40c3ba6a3eb97df229
Clin Shaiju <63211630+ClinShaiju@users.noreply.github.com>
2025-01-12 23:09:11 -0600

    Create temp pages to be filled

---
+++ src/views/AccessibilityView.vue
@@ -0,0 +1,110 @@
+<script setup></script>
+<template>
+  <div class="min-h-[calc(100vh-80px)] w-full bg-neutral-950 text-white">
+    <div class="flex flex-col items-center">
+      <div class="h-[80px]"></div>
+      <div class="flex flex-col max-w-screen-lg p-8 gap-y-4">
+        <h2 class="text-3xl font-bold">
+          Accessibility Statement for <span class="">Stage Fright</span>
+        </h2>
+        <p>
+          This is an accessibility statement from
+          <span class="">Stage Fright</span>.
+        </p>
+        <h3 class="text-xl font-bold">Measures to support accessibility</h3>
+        <p>
+          <span class="">Stage Fright</span>
+          takes the following measures to ensure accessibility of
+          <span class="">Stage Fright</span>:
+        </p>
+        <ul class="">
+          <li>Include accessibility throughout our internal policies.</li>
+          <li>Integrate accessibility into our procurement practices.</li>
+        </ul>
+        <h3 class="text-xl font-bold">Conformance status</h3>
+        <p>
+          The
+          <a href="https://www.w3.org/WAI/standards-guidelines/wcag/"
+            >Web Content Accessibility Guidelines (WCAG)</a
+          >
+          defines requirements for designers and developers to improve accessibility for people with
+          disabilities. It defines three levels of conformance: Level A, Level AA, and Level AAA.
+          <span class="">Stage Fright</span>
+          is
+          <span class="" data-printfilter="lowercase">fully conformant</span>
+          with
+          <span class=""><span data-negate="">WCAG 2.0 level AA</span>.</span>
+          <span>
+            <span class="">Fully conformant</span>
+            means that
+            <span class=""
+              >the content fully conforms to the accessibility standard without any exceptions</span
+            >.
+          </span>
+        </p>
+        <h3 class="text-xl font-bold">Feedback</h3>
+        <p>
+          We welcome your feedback on the accessibility of
+          <span class="">Stage Fright</span>. Please let us know if you encounter accessibility
+          barriers on <span class="">Stage Fright</span>:
+        </p>
+        <ul class="">
+          <li>
+            Phone:
+            <span class="">(832) 329 2275</span>
+          </li>
+          <li>
+            E-mail:
+            <a class="" href="mailto:feedback@stagefrightrock.com">feedback@stagefrightrock.com</a>
+          </li>
+          <li>
+            Visitor Address:
+            <span class="">10300 Eagle Dr., Mont Belvieu TX 77523</span>
+          </li>
+        </ul>
+        <p>
+          We try to respond to feedback within
+          <span class="">2 business days</span>.
+        </p>
+        <h3 class="text-xl font-bold">Technical specifications</h3>
+        <p>
+          Accessibility of
+          <span class="">Stage Fright</span>
+          relies on the following technologies to work with the particular combination of web
+          browser and any assistive technologies or plugins installed on your computer:
+        </p>
+        <ul class="">
+          <li>HTML</li>
+          <li>WAI-ARIA</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+        </ul>
+        <p>
+          These technologies are relied upon for conformance with the accessibility standards used.
+        </p>
+        <h3 class="text-xl font-bold">Assessment approach</h3>
+        <p>
+          <span class="">Stage Fright</span>
+          assessed the accessibility of
+          <span class="">Stage Fright</span>
+          by the following approaches:
+        </p>
+        <ul class="">
+          <li>Self-evaluation</li>
+        </ul>
+        <h3 class="text-xl font-bold">Formal complaints</h3>
+        <p class="">Send us your complaints at feedback@stagefrightrock.com</p>
+        <hr noshade="noshade" />
+        <h3 class="text-xl font-bold">Date</h3>
+        <p>
+          This statement was created on
+          <span class="">7 January 2025</span>
+          using the
+          <a href="https://www.w3.org/WAI/planning/statements/"
+            >W3C Accessibility Statement Generator Tool</a
+          >.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ src/views/AttributionsView.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+<template>
+  <div class="bg-black h-dvh flex flex-col items-center justify-center p-8">
+    <div class="h-[80px] w-dvw"></div>
+    <h1 class="text-white font-thedark text-6xl mb-10">Merch</h1>
+
+    <object
+      class="pdf max-w-screen-2xl w-full h-full bg-white"
+      data="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20210101201653/PDF.pdf"
+      aria-label="Attributions PDF"
+    ></object>
+  </div>
+</template>

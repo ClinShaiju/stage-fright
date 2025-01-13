d231d02d6b0b3b0c1d767a6493e1dd8102d63f17
Clin Shaiju <63211630+ClinShaiju@users.noreply.github.com>
2025-01-12 23:09:19 -0600

    Clean up contact view

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,15 +1,35 @@
+<script setup>
+import MiniPopup from '@/components/MiniPopup.vue'
+import { ref } from 'vue'
+
+// Reactive states for popup management
+const popuped = ref(false) // Controls visibility of the popup
+const popupText = ref('') // Text content of the popup
+
+// Function to trigger the popup
+async function popup(text) {
+  popupText.value = text // Set the text for the popup
+  popuped.value = true // Show the popup
+
+  const timeout = setTimeout(() => {
+    popuped.value = false // Hide the popup after 2.5 seconds
+    clearTimeout(timeout) // Clear the timeout to prevent memory leaks
+  }, 2000)
+}
+</script>
+
 <template>
-  <div class="flex flex-col bg-neutral-950 items-center min-h-dvh ">
-    <div class="h-[80px]"></div>
-    <div class="mt-8 flex flex-col items-center justify-end ">
-      <h1 class="flex text-white h-auto w-dvw text-6xl justify-center  font-thedark">
+  <div class="flex flex-col bg-neutral-900 items-center min-h-dvh ">
+    <div class="h-[80px] w-dvw"></div>
+    <div class="mt-8 flex flex-col items-center justify-end mb-16">
+      <MiniPopup :show="popuped" :text="popupText" @close="popuped = false"></MiniPopup>
+      <h1 class="flex text-white h-auto w-dvw text-6xl justify-center font-thedark">
         Contact Us
       </h1>
-      <h2 class="flex text-white h-auto text-1.5xl w-dvw justify-center  font-loud">
+      <h2 class="flex text-white h-auto text-2xl w-dvw justify-center  font-loud text-center">
         Leave a message below or contact us directly!
       </h2>
     </div>
-
     <div class="flex flex-col-reverse  w-dvw max-w-7xl  md:flex-row  ">
       <div class="flex flex-col w-full md:w-1/2  p-6 text-center">
         <h2 class="flex text-6xl text-white font-thedark justify-center">Online Inquiry</h2>
@@ -37,9 +57,10 @@
           ></textarea>
 
           <button
-            type="submit"
+            type="button"
             class="transition-all hover:scale-[1.1] px-24 py-2.5 text-xl text-white font-semibold rounded-full border border-white hover:text-white hover:bg-red-700 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 w-auto"
-          >
+          @click="popup('Your message has been submitted!')"
+            >
             Submit
           </button>
         </form>

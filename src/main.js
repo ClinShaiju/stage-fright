ec7fbc099c2df2bde0875bdf51d5f52bc4ef5d6f
Clin Shaiju <63211630+ClinShaiju@users.noreply.github.com>
2025-01-14 10:15:20 -0600

    Remove vue-gtag from app

--- src/main.js
+++ src/main.js
@@ -2,17 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import router from './router'
-import VueGtag from "vue-gtag";
-
-Vue.config.productionTip = false;
 
 const pinia = createPinia()
 const app = createApp(App)
 
 app.use(router)
 app.use(pinia)
-Vue.use(VueGtag, {
-    config: { id: "G-SEB3KY3RB7" }
-  });
 
 app.mount('#app')

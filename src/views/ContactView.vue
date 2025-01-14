9575b2dd1ed66ec72bf4e8a87365b7d548fb9b99
Joshua Zamora <180025124+joshmzamora@users.noreply.github.com>
2025-01-13 20:10:09 -0600

    Added FAQs to Contact page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -2,87 +2,171 @@
 import MiniPopup from '@/components/MiniPopup.vue'
 import { ref } from 'vue'
 
-// Reactive states for popup management
-const popuped = ref(false) // Controls visibility of the popup
-const popupText = ref('') // Text content of the popup
+const popuped = ref(false)
+const popupText = ref('')
 
-// Function to trigger the popup
 async function popup(text) {
-  popupText.value = text // Set the text for the popup
-  popuped.value = true // Show the popup
+  popupText.value = text
+  popuped.value = true
 
   const timeout = setTimeout(() => {
-    popuped.value = false // Hide the popup after 2.5 seconds
-    clearTimeout(timeout) // Clear the timeout to prevent memory leaks
+    popuped.value = false
+    clearTimeout(timeout)
   }, 2000)
 }
 </script>
 
 <template>
-  <div class="flex flex-col bg-neutral-900 items-center min-h-dvh ">
+  <div class="flex flex-col bg-neutral-900 items-center min-h-dvh">
     <div class="h-[80px] w-dvw"></div>
-    <div class="mt-8 flex flex-col items-center justify-end mb-16">
+    <div class="mt-8 flex flex-col items-center justify-end mb-8">
       <MiniPopup :show="popuped" :text="popupText" @close="popuped = false"></MiniPopup>
-      <h1 class="flex text-white h-auto w-dvw text-6xl justify-center font-thedark">
-        Contact Us
-      </h1>
-      <h2 class="flex text-white h-auto text-2xl w-dvw justify-center  font-loud text-center">
+      <h1 class="flex text-white h-auto w-dvw text-6xl justify-center font-thedark">Contact Us</h1>
+      <h2 class="flex text-white h-auto text-2xl w-dvw justify-center font-loud text-center">
         Leave a message below or contact us directly!
       </h2>
     </div>
-    <div class="flex flex-col-reverse  w-dvw max-w-7xl  md:flex-row  ">
-      <div class="flex flex-col w-full md:w-1/2  p-6 text-center">
-        <h2 class="flex text-6xl text-white font-thedark justify-center">Online Inquiry</h2>
-        <h2 class="flex text-xl text-white font-thin justify-center mt-3 ">Our team will get back to you soon!</h2>
+    <div class="flex flex-col-reverse w-dvw max-w-7xl md:flex-row">
+      <div class="flex flex-col w-full md:w-1/2 p-6 text-center">
+        <h2 class="flex text-5xl text-white font-loud justify-center">Online Inquiry</h2>
+        <h2 class="flex text-xl text-white font-thin justify-center mt-3">
+          Our team will get back to you soon!
+        </h2>
         <form class="flex flex-col items-center m-4 gap-4">
           <input
             id="name"
             type="text"
-            class="block  rounded-md p-2 text-black w-full"
+            class="block rounded-md p-2 text-black w-full"
             placeholder="Name"
           />
-
           <input
-            id="name"
+            id="email"
             type="text"
-            class="block  rounded-md p-2 text-black w-full"
+            class="block rounded-md p-2 text-black w-full"
             placeholder="Email"
           />
-
           <textarea
             id="message"
-            type="text"
-            class="block  rounded-md p-2 text-black w-full h-32 md:h-40 "
+            class="block rounded-md p-2 text-black w-full h-32 md:h-40"
             placeholder="Message"
           ></textarea>
-
           <button
             type="button"
             class="transition-all hover:scale-[1.1] px-24 py-2.5 text-xl text-white font-semibold rounded-full border border-white hover:text-white hover:bg-red-700 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 w-auto"
-          @click="popup('Your message has been submitted!')"
-            >
+            @click="popup('Your message has been submitted!')"
+          >
             Submit
           </button>
         </form>
       </div>
       <div class="w-full md:w-1/2 p-6 text-center">
-        <h2 class="flex text-6xl text-white font-thedark justify-center">Contact Details</h2>
+        <h2 class="flex text-5xl text-white font-loud justify-center">Contact Details</h2>
         <div class="mb-4">
           <h2 class="flex text-2xl text-white justify-center font-bold mb-1 mt-3">Support</h2>
-          <h2 class="flex text-xl text-white font-thin justify-center hover:underline hover:text-red-600 hover:font-bold transition-all"><a href="mailto:support@stagefrightrock.com" title="Stage Fright's support email">support@stagefrightrock.com</a></h2>
+          <h2
+            class="flex text-xl text-white font-thin justify-center hover:underline hover:text-red-600 hover:font-bold transition-all"
+          >
+            <a href="mailto:support@stagefrightrock.com" title="Stage Fright's support email"
+              >support@stagefrightrock.com</a
+            >
+          </h2>
         </div>
         <div class="mb-4">
           <h2 class="flex text-2xl text-white justify-center font-bold mb-1">Feedback</h2>
-          <h2 class="flex text-xl text-white font-thin justify-center hover:underline hover:text-red-600 hover:font-bold transition-all"><a href="mailto:feedback@stagefrightrock.com" title="Stage Fright's support email">feedback@stagefrightrock.com</a></h2>
+          <h2
+            class="flex text-xl text-white font-thin justify-center hover:underline hover:text-red-600 hover:font-bold transition-all"
+          >
+            <a href="mailto:feedback@stagefrightrock.com" title="Stage Fright's support email"
+              >feedback@stagefrightrock.com</a
+            >
+          </h2>
         </div>
         <div class="mb-4">
           <h2 class="flex text-2xl text-white justify-center font-bold mb-1">Booking</h2>
-          <h2 class="flex text-xl text-white font-thin justify-center hover:underline hover:text-red-600 hover:font-bold transition-all"><a href="mailto:booking@stagefrightrock.com" title="Stage Fright's support email">booking@stagefrightrock.com</a></h2>
+          <h2
+            class="flex text-xl text-white font-thin justify-center hover:underline hover:text-red-600 hover:font-bold transition-all"
+          >
+            <a href="mailto:booking@stagefrightrock.com" title="Stage Fright's support email"
+              >booking@stagefrightrock.com</a
+            >
+          </h2>
         </div>
         <div class="mb-4">
           <h2 class="flex text-2xl text-white justify-center font-bold mb-1">Phone Number</h2>
-          <h2 class="flex text-xl text-white font-thin justify-center hover:underline hover:text-red-600 hover:font-bold transition-all"><a href="tel:+8329842275" title="Stage Fright's phone number">(832) 329-ROCK</a> </h2>
+          <h2
+            class="flex text-xl text-white font-thin justify-center hover:underline hover:text-red-600 hover:font-bold transition-all"
+          >
+            <a href="tel:+8329842275" title="Stage Fright's phone number">(832) 329-ROCK</a>
+          </h2>
         </div>
+      </div>
+    </div>
+
+    <h2 class="text-white font-loud text-5xl p-16 text-center">Frequently Asked Questions</h2>
+
+    <div class="flex flex-col px-8 ">
+      <div
+        class="max-w-3xl my-4 space-y-6 bg-gray-200 rounded-lg p-8 hover:scale-105 transition-all"
+      >
+        <p class="font-bold text-black text-[22px]">
+          How can I book Stage Fright for a concert or event?
+        </p>
+        <p class="text-black">
+          Booking Stage Fright for your event is easy! Simply email our booking team with details
+          about your event, including date, location, and expected audience size. We’ll get back to
+          you with availability and pricing information.
+        </p>
+      </div>
+
+      <div
+        class="max-w-3xl my-4 space-y-6 bg-gray-200 rounded-lg p-8 hover:scale-105 transition-all"
+      >
+        <p class="font-bold text-black text-[22px]">
+          Can I request a song for the band to perform at a show?
+        </p>
+        <p class="text-black">
+          While we can’t accommodate every song request, we do occasionally take fan requests for
+          specific songs during our concerts. Be sure to check our setlist for the tour, and feel
+          free to shout your request at the show!
+        </p>
+      </div>
+
+      <div
+        class="max-w-3xl my-4 space-y-6 bg-gray-200 rounded-lg p-8 hover:scale-105 transition-all"
+      >
+        <p class="font-bold text-black text-[22px]">
+          How do I stay updated with Stage Fright news and releases?
+        </p>
+        <p class="text-black">
+          The best way to stay in the loop is to follow us on our social media platforms (Instagram,
+          Youtube, Facebook) and subscribe to our newsletter. We send out the latest news, album
+          releases, tour dates, and exclusive content to our fans!
+        </p>
+      </div>
+
+      <div
+        class="max-w-3xl my-4 space-y-6 bg-gray-200 rounded-lg p-8 hover:scale-105 transition-all"
+      >
+        <p class="font-bold text-black text-[22px]">
+          What makes Stage Fright different from other bands in the death metal scene?
+        </p>
+        <p class="text-black">
+          Stage Fright has become a global powerhouse by blending technical musicianship, raw
+          emotion, and a relentless live presence. We push the boundaries of extreme metal while
+          maintaining a deep connection with our dedicated fanbase. Our shows are known for their
+          intensity, energy, and unforgettable atmosphere.
+        </p>
+      </div>
+
+      <div
+        class="max-w-3xl my-4 space-y-6 bg-gray-200 rounded-lg p-8 hover:scale-105 transition-all"
+      >
+        <p class="font-bold text-black text-[22px]">What is the band's tour schedule?</p>
+        <p class="text-black">
+          Our global tour schedule is updated regularly on our website and social media channels.
+          Follow us on Instagram, Youtube, and Facebook to stay updated on our upcoming shows near
+          you!
+        </p>
       </div>
     </div>
   </div>
--- src/views/MusicView.vue
+++ src/views/MusicView.vue
@@ -1,8 +1,161 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
-  <div class="bg-black h-dvh flex">
-    <h1 class="flex text-white h-dvh w-dvw items-center justify-center text-3xl">Music</h1>
+  <div class="flex min-h-screen flex-col items-center justify-center bg-stone-900">
+    <div class="h-[80px]"></div>
+    <h1 class="justify-center font-thedark text-6xl text-white">Listen Now</h1>
+    <h2 class="p-4 pl-10 pr-10 text-center font-thin text-white">
+      Stage Fright’s discography is a journey through raw emotion, powerful storytelling, and
+      electrifying energy. Dive into their track samples below and experience the highs, the lows,
+      and everything in between.
+    </h2>
+    <div class="flex w-auto flex-col items-center justify-center bg-black p-4 py-16 md:p-8">
+      <div class="flex h-full w-dvw flex-col items-center justify-around gap-8 lg:flex-row">
+        <div
+          class="flex h-auto w-fit flex-col overflow-hidden rounded-md border border-neutral-700 transition-all hover:scale-105"
+        >
+          <p class="p-8 text-center font-loud text-5xl uppercase text-white underline">Escape</p>
+          <div class="flex justify-center">
+            <img
+              src="/public/assets/images/escape.jpg"
+              alt="'Escape' Album Cover"
+              class="h-fit max-w-80"
+            />
+          </div>
+
+          <div class="flex justify-center p-6">
+            <div
+              class="flex w-full max-w-screen-lg flex-col items-center justify-center rounded-lg bg-stone-800 p-8 shadow-lg"
+            >
+              <h1 class="p-6 text-center font-thedark text-4xl text-white">Edge of Night</h1>
+              <audio controls class="w-full">
+                <source src="/public/assets/music/Breaking Free.mp3" type="audio/mp3" />
+              </audio>
+              <div class="p-6">
+                <button
+                  class="font:thin w-full rounded-lg bg-stone-400 p-6 px-4 py-2 text-white transition-all hover:scale-105 hover:bg-stone-500 active:bg-stone-700"
+                >
+                  About the album
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="flex h-auto w-fit flex-col overflow-hidden rounded-md border border-neutral-700 transition-all hover:scale-105"
+        >
+          <p class="p-8 text-center font-loud text-5xl uppercase text-white underline">
+            Scared to Death
+          </p>
+          <div class="flex justify-center">
+            <img
+              src="/public/assets/images/scared_to_death.jpg"
+              alt="'Escape' Album Cover"
+              class="h-fit max-w-80"
+            />
+          </div>
+          <div class="flex justify-center p-6">
+            <div
+              class="flex w-full max-w-screen-lg flex-col items-center justify-center rounded-lg bg-stone-800 p-8 shadow-lg"
+            >
+              <h1 class="p-6 text-center font-thedark text-4xl text-white">Ring the Alarm</h1>
+              <audio controls class="w-full">
+                <source src="/public/assets/music/Edge of Night.mp3" type="audio/mp3" />
+              </audio>
+              <div class="p-6">
+                <button
+                  class="font:thin w-full rounded-lg bg-stone-400 p-6 px-4 py-2 text-white transition-all hover:scale-105 hover:bg-stone-500 active:bg-stone-700"
+                >
+                  About the album
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="flex h-auto w-fit flex-col overflow-hidden rounded-md border border-neutral-700 transition-all hover:scale-105"
+        >
+          <p class="p-8 text-center font-loud text-5xl uppercase text-white underline">
+            Endorphins
+          </p>
+          <div class="flex justify-center">
+            <img
+              src="/public/assets/images/endorphins.jpg"
+              alt="'Escape' Album Cover"
+              class="h-fit max-w-80"
+            />
+          </div>
+          <div class="flex justify-center p-6">
+            <div
+              class="flex w-full max-w-screen-lg flex-col items-center justify-center rounded-lg bg-stone-800 p-8 shadow-lg"
+            >
+              <h1 class="p-6 text-center font-thedark text-4xl text-white">Alleviate</h1>
+              <audio controls class="w-full">
+                <source src="/public/assets/music/Unstoppable Force.mp3" type="audio/mp3" />
+              </audio>
+              <div class="p-6">
+                <button
+                  class="font:thin w-full rounded-lg bg-stone-400 p-6 px-4 py-2 text-white transition-all hover:scale-105 hover:bg-stone-500 active:bg-stone-700"
+                >
+                  About the album
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <a href="https://open.spotify.com/user/314pmfz6ftbrlmjmgmtacdd5ntx4">
+        <button
+          class="mt-16 w-auto rounded-md border border-neutral-700 bg-black p-4 text-3xl text-white transition-all hover:scale-105 hover:bg-neutral-900"
+        >
+          Check out our Spotify
+        </button>
+      </a>
+    </div>
+
+    <h1 class="pt-10 p-4 justify-center items-center text-center font-thedark text-6xl text-white">
+      Scared to Death
+    </h1>
+    <h2 class="p-4 pl-10 pr-10 text-center font-thin text-white">
+      Fear isn’t the enemy—it’s the fuel that drives us to the edge and beyond.
+    </h2>
+    <div class="grid grid-cols-1 gap-10 gap-x-12 pb-8 pt-1 lg:grid-cols-2">
+      <div class="">
+        <h2 class="p-1 font-grunge text-4xl text-white first-letter:text-5xl hover:text-stone-300">
+          1. Ring the Alarm
+        </h2>
+        <h2 class="p-1 font-grunge text-4xl text-white first-letter:text-5xl hover:text-stone-300">
+          2. Ring the Alarm
+        </h2>
+        <h2 class="p-1 font-grunge text-4xl text-white first-letter:text-5xl hover:text-stone-300">
+          3. Ring the Alarm
+        </h2>
+        <h2 class="p-1 font-grunge text-4xl text-white first-letter:text-5xl hover:text-stone-300">
+          4. Ring the Alarm
+        </h2>
+        <h2 class="p-1 font-grunge text-4xl text-white first-letter:text-5xl hover:text-stone-300">
+          5. Ring the Alarm
+        </h2>
+      </div>
+      <div class="">
+        <h2 class="p-1 font-grunge text-4xl text-white first-letter:text-5xl hover:text-stone-300">
+          6. Ring the Alarm
+        </h2>
+        <h2 class="p-1 font-grunge text-4xl text-white first-letter:text-5xl hover:text-stone-300">
+          7. Ring the Alarm
+        </h2>
+        <h2 class="p-1 font-grunge text-4xl text-white first-letter:text-5xl hover:text-stone-300">
+          8. Ring the Alarm
+        </h2>
+        <h2 class="p-1 font-grunge text-4xl text-white first-letter:text-5xl hover:text-stone-300">
+          9. Ring the Alarm
+        </h2>
+        <h2 class="p-1 font-grunge text-4xl text-white first-letter:text-5xl hover:text-stone-300">
+          10. Ring the Alarm
+        </h2>
+      </div>
+    </div>
+
+    <div></div>
   </div>
 </template>

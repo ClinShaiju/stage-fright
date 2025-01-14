daecbd492153a1fcee33a9c1b7a7f296d0fe5ab2
Clin Shaiju <63211630+ClinShaiju@users.noreply.github.com>
2025-01-14 10:39:06 -0600

    Rename Attributions page header

--- src/views/AttributionsView.vue
+++ src/views/AttributionsView.vue
@@ -2,7 +2,7 @@
 <template>
   <div class="bg-black h-dvh flex flex-col items-center justify-center p-8">
     <div class="h-[80px] w-dvw"></div>
-    <h1 class="text-white font-thedark text-6xl mb-10">Merch</h1>
+    <h1 class="text-white font-thedark text-6xl mb-10">Attributions</h1>
 
     <object
       class="pdf max-w-screen-2xl w-full h-full bg-white"

0338b527ffba7e2e782733ced46ee98477bf0eb8
Clin Shaiju <63211630+ClinShaiju@users.noreply.github.com>
2025-01-14 22:43:22 -0600

    Clean up MusicView to match site design

--- src/components/ModalPopup.vue
+++ src/components/ModalPopup.vue
@@ -10,7 +10,7 @@ props;
 <template>
   <Transition name="fade">
     <div v-if="show" class="flex fixed bg-black bg-opacity-65 w-dvw h-dvh z-[100] items-center justify-center md:p-16 top-0" @click.self="$emit('close')">
-      <div class=" flex flex-col items-start max-w-screen-2xl w-full h-full bg-black border border-neutral-800 bg-opacity-100 p-4 md:p-8">
+      <div class=" flex flex-col items-start max-w-screen-2xl w-full h-full bg-black border border-neutral-800 bg-opacity-100 p-4 md:p-8 rounded-lg">
         <div class="flex justify-end w-full">
 
           <button @click="$emit('close')" class="text-white hover:bg-neutral-900 rounded-md transition-all duration-200">
---
+++ src/music.js
@@ -0,0 +1,3 @@
+const musicInfo = new Map()
+
+export { musicInfo }

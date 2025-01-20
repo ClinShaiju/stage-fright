9f04beb504eb4c0293c8d91adce26e7328ef51c0
Clin Shaiju <63211630+ClinShaiju@users.noreply.github.com>
2025-01-20 17:21:28 -0600

    Update ticket price ID and fix router transition failing

--- src/components/MiniPopup.vue
+++ src/components/MiniPopup.vue
@@ -2,21 +2,23 @@
 const props = defineProps({
   show: Boolean,
   text: String,
-});
+})
 props
 </script>
 
 <template>
-  <Transition name="fade">
-    <div
-      v-show="show"
-      class="fixed m-8 z-[100] md:right-0 md-right-1/2 flex flex-row w-72 h-16 bg-green-200 bg-opacity-100 rounded-md border-green-700 border border-r-3 border-b-4 items-center justify-between p-4 hover:cursor-pointer"
-      @click="$emit('close')"
-    >
-      <p class="text-green-800 font-bold">{{ text }}</p>
-      <button class="text-green-800 font-bold"><a>X</a></button>
-    </div>
-  </Transition>
+  <div>
+    <Transition name="fade">
+      <div
+        v-show="show"
+        class="fixed m-8 z-[100] md:right-0 md-right-1/2 flex flex-row w-72 h-16 bg-green-200 bg-opacity-100 rounded-md border-green-700 border border-r-3 border-b-4 items-center justify-between p-4 hover:cursor-pointer"
+        @click="$emit('close')"
+      >
+        <p class="text-green-800 font-bold">{{ text }}</p>
+        <button class="text-green-800 font-bold"><a>X</a></button>
+      </div>
+    </Transition>
+  </div>
 </template>
 
 <style>
--- src/components/ModalPopup.vue
+++ src/components/ModalPopup.vue
@@ -2,28 +2,41 @@
 const props = defineProps({
   show: Boolean,
   heading: String,
-  text: String
+  text: String,
 })
-props;
+props
 </script>
 
 <template>
-  <Transition name="fade">
-    <div v-if="show" class="flex fixed bg-black bg-opacity-65 w-dvw h-dvh z-[100] items-center justify-center md:p-16 top-0" @click.self="$emit('close')">
-      <div class=" flex flex-col items-start max-w-screen-2xl w-full h-full bg-black border border-neutral-800 bg-opacity-100 p-4 md:p-8 rounded-lg">
-        <div class="flex justify-end w-full">
-
-          <button @click="$emit('close')" class="text-white hover:bg-neutral-900 rounded-md transition-all duration-200">
-            <img src="/assets/icons/close.svg" alt="Close popup" class="w-12 invert brightness-0">
-          </button>
+  <div>
+    <Transition name="fade">
+      <div
+        v-if="show"
+        class="flex fixed bg-black bg-opacity-65 w-dvw h-dvh z-[100] items-center justify-center md:p-16 top-0"
+        @click.self="$emit('close')"
+      >
+        <div
+          class="flex flex-col items-start max-w-screen-2xl w-full h-full bg-black border border-neutral-800 bg-opacity-100 p-4 md:p-8 rounded-lg"
+        >
+          <div class="flex justify-end w-full">
+            <button
+              @click="$emit('close')"
+              class="text-white hover:bg-neutral-900 rounded-md transition-all duration-200"
+            >
+              <img
+                src="/assets/icons/close.svg"
+                alt="Close popup"
+                class="w-12 invert brightness-0"
+              />
+            </button>
+          </div>
+          <div class="w-full h-full overflow-scroll md:px-8 flex flex-col items-center">
+            <slot class="w-full"></slot>
+          </div>
         </div>
-        <div class="w-full h-full overflow-scroll md:px-8 flex flex-col items-center">
-          <slot class="w-full"></slot>
-        </div>
-
       </div>
-    </div>
-  </Transition>
+    </Transition>
+  </div>
 </template>
 
 <style>
